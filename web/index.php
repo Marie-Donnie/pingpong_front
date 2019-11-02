f13b61d97eefb13e2f9b3cf882143d7915ff3f74
Josie Bealle <josiebealle@gmail.com>
2019-11-02 15:16:01 +0100

    table

--- web/index.php
+++ web/index.php
@@ -42,7 +42,7 @@
             console.log(data)
             $.post("https://aqueous-dusk-24314.herokuapp.com/ip/add", {ip: data}, function(resp, status)
             {
-             ipdiv.append("<tr><td>\t" + resp.address + "\t</td><td>\t" + resp.latitude + "\t</td><td>\t" + resp.longitude + "\t</td></tr>");
+             ipdiv.append("<tr><td>" + resp.address + "</td><td>" + resp.latitude + "</td><td>" + resp.longitude + "</td></tr>");
 
             });
           } )
@@ -51,19 +51,22 @@
 
           var ip_data = $.get("https://aqueous-dusk-24314.herokuapp.com/ip/all", function(data, status) {
             data.forEach((addr) => {
-                ipdiv.append("<tr><td>\t" + addr.address + "\t</td><td>\t" + addr.latitude + "\t</td><td>\t" + addr.longitude + "\t</td></tr>");
+                ipdiv.append("<tr><td>" + addr.address + "</td><td>" + addr.latitude + "</td><td>" + addr.longitude + "</td></tr>");
                 });
           });
       });
     </script>
+    <style type="text/css">
+        #ip_data td { padding:10px; }
+    </style>
     <h2>Cliquez le bouton pour partager votre addresse IP avec nous</h2>
     <button id="postIP">Partage mon ip</button>
     <div>
         <table id="ip_data">
             <tr>
-                <th>    Addresse IP </th>
-                <th>    Latitude    </th>
-                <th>    Longitude   </th>
+                <th>Addresse IP</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
             </tr>
         </table>
     </div>
