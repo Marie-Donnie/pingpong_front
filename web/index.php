b25351876c61e8a84afe90558a0e8cfc29bf59f3
Josie Bealle <josiebealle@gmail.com>
2019-12-19 11:21:26 +0100

    put in real python

--- web/index.php
+++ web/index.php
@@ -45,8 +45,8 @@
       <hr class="my-4">
       <p class="lead">Cliquez le bouton pour partager votre addresse IP avec nous.</p>
       <button class="btn btn-primary btn-lg mb-3" id="postIP">Partager mon ip</button>
-      <p class="lead">Cliquez le bouton pour télécharger le client python afin de lancer des requêtes traceroutes. Lors du complétion du téléchargement, veuillez lancer le programme. Lorsque son execution est terminée, vous pouvez supprimer le fichier. Merci pour votre participation ! </p>
-      <a href="p/" download="pingpong.py" class="btn btn-dark btn-lg">Télécharger client </a>
+      <p class="lead">Cliquez le bouton pour télécharger le client python afin de lancer des requêtes traceroutes. Lors du complétion du téléchargement, veuillez unzipper le programme, et executer le ficher éxécutable dans le dossier. Lorsque son éxécution est terminée, vous pouvez supprimer le téléchargement. Merci pour votre participation ! </p>
+      <a href="p/" download="Interface.zip" class="btn btn-dark btn-lg">Télécharger client </a>
     </div>
   </body>
 </html>
