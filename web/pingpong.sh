7cc143b71443b2b939d3bb41aefb8488b23d69a0
Josie Bealle <josiebealle@gmail.com>
2020-01-20 18:56:00 +0100

    change sh

--- web/pingpong.sh
+++ web/pingpong.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo " tracerouting from my IP adrress : "
-curl ifconfig.me
-
+echo "%SRC%" >> save.txt
+curl ifconfig.me >> save.txt
+echo "%END_SRC%" >> save.txt
 echo "      "
 
 str=$(curl http://aqueous-dusk-24314.herokuapp.com/ip/all/address_only)
@@ -12,8 +13,9 @@ read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
 for i in "${ADDR[@]}"; do # access each element of array
    echo "Tracerouting to"
    echo $i
-
+   echo "%BEGIN_TR%" >> save.txt
    traceroute -m 20 -w 1 -q 1 $i >> save.txt
+   echo "%END_TR%" >> save.txt
 
 
 done
