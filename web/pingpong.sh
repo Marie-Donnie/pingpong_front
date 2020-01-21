7e7375669c8c969b67f8da38ef133aacdabdadaa
Josie Bealle <josiebealle@gmail.com>
2020-01-21 10:12:51 +0100

    better delimiters

--- web/pingpong.sh
+++ web/pingpong.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 echo " tracerouting from my IP adrress : "
-echo "%SRC%" >> save.txt
+echo "__SRC__" >> save.txt
 curl ifconfig.me >> save.txt
-echo "%END_SRC%" >> save.txt
+echo "__END_SRC__" >> save.txt
 echo "      "
 
 str=$(curl http://aqueous-dusk-24314.herokuapp.com/ip/all/address_only)
 
 IFS=',' # hyphen (-) is set as delimiter
 read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
+counter=0;
 for i in "${ADDR[@]}"; do # access each element of array
    echo "Tracerouting to"
    echo $i
-   echo "%BEGIN_TR%" >> save.txt
+   echo "__BEGIN_" >> save.txt
+   echo $counter >> save.txt
+   echo "_TR__">>save.txt
+   echo "__DST__" >> save.txt
+   echo $i >> save.txt
+   echo "__END_DST__" >> save.txt
    traceroute -m 20 -w 1 -q 1 $i >> save.txt
-   echo "%END_TR%" >> save.txt
-
-
+   echo "__END_" >> save.txt
+   echo $counter >> save.txt
+   echo "_TR__" >> save.txt
+   counter=$((counter + 1))
 done
 
 curl -X POST -d @save.txt https://aqueous-dusk-24314.herokuapp.com/traceroute/
