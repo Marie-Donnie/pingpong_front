5fdc220d4e805307cbe41e603ff78055bb6d32b8
Josie Bealle <josiebealle@gmail.com>
2020-01-29 12:13:48 +0100

    install curl

--- web/pingpong.sh
+++ web/pingpong.sh
@@ -1,37 +1,39 @@
-rm -f pingpong.txt
-if command -v curl >/dev/null 2>&1 ; then
-    #echo "curl found"
-    echo " tracerouting from my IP adrress : "
-    echo "__SRC__" >> pingpong.txt
-    curl ifconfig.me >> pingpong.txt
-    echo "__END_SRC__" >> pingpong.txt
-    echo "      "
+perform_tr () {
+  IFS=',' # hyphen (-) is set as delimiter
+  read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
+  counter=0;
+  for i in "${ADDR[@]}"; do
+     echo "Tracerouting to"
+     echo $i
+     echo "__BEGIN_" >> pingpong.txt
+     echo $counter >> pingpong.txt
+     echo "_TR__">>pingpong.txt
+     echo "__DST__" >> pingpong.txt
+     echo $i >> pingpong.txt
+     echo "__END_DST__" >> pingpong.txt
+     traceroute -m 20 -w 1 -q 1 $i >> pingpong.txt
+     echo "__END_" >> pingpong.txt
+     echo $counter >> pingpong.txt
+     echo "_TR__" >> pingpong.txt
+     counter=$((counter + 1))
+  done
+}
 
-    str=$(curl http://aqueous-dusk-24314.herokuapp.com/ip/all/address_only)
+pingpong_curl () {
+  echo " tracerouting from my IP adrress : "
+      echo "__SRC__" >> pingpong.txt
+      curl ifconfig.me >> pingpong.txt
+      echo "__END_SRC__" >> pingpong.txt
+      echo "      "
 
-    IFS=',' # hyphen (-) is set as delimiter
-    read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
-    counter=0;
-    for i in "${ADDR[@]}"; do
-       echo "Tracerouting to"
-       echo $i
-       echo "__BEGIN_" >> pingpong.txt
-       echo $counter >> pingpong.txt
-       echo "_TR__">>pingpong.txt
-       echo "__DST__" >> pingpong.txt
-       echo $i >> pingpong.txt
-       echo "__END_DST__" >> pingpong.txt
-       traceroute -m 20 -w 1 -q 1 $i >> pingpong.txt
-       echo "__END_" >> pingpong.txt
-       echo $counter >> pingpong.txt
-       echo "_TR__" >> pingpong.txt
-       counter=$((counter + 1))
-    done
+      str=$(curl http://aqueous-dusk-24314.herokuapp.com/ip/all/address_only)
 
-    curl -X POST -d @pingpong.txt https://aqueous-dusk-24314.herokuapp.com/traceroute/
-   
-else
-    #echo "curl not found"
+      perform_tr $str
+
+      curl -X POST -d @pingpong.txt https://aqueous-dusk-24314.herokuapp.com/traceroute/
+}
+
+pingpong_wget () {
     echo " tracerouting from my IP adrress : "
     echo "__SRC__" >> pingpong.txt
     #wget --output-document=pingpong.txt ifconfig.me
@@ -45,24 +47,24 @@ else
     str=$(wget -qO- http://aqueous-dusk-24314.herokuapp.com/ip/all/address_only)
     echo $str
 
-    IFS=',' # hyphen (-) is set as delimiter
-    read -ra ADDR <<< "$str" # str is read into an array as tokens separated by IFS
-    counter=0;
-    for i in "${ADDR[@]}"; do
-       echo "Tracerouting to"
-       echo $i
-       echo "__BEGIN_" >> pingpong.txt
-       echo $counter >> pingpong.txt
-       echo "_TR__">>pingpong.txt
-       echo "__DST__" >> pingpong.txt
-       echo $i >> pingpong.txt
-       echo "__END_DST__" >> pingpong.txt
-       traceroute -m 20 -w 1 -q 1 $i >> pingpong.txt
-       echo "__END_" >> pingpong.txt
-       echo $counter >> pingpong.txt
-       echo "_TR__" >> pingpong.txt
-       counter=$((counter + 1))
-    done
+    perform_tr $str
 
     wget --post-file=pingpong.txt https://aqueous-dusk-24314.herokuapp.com/traceroute/
+}
+
+rm -f pingpong.txt
+
+if command -v curl >/dev/null 2>&1 ; then
+    pingpong_curl
+   
+elif command -v wget >/dev/null 2>&1; then
+    pingpong_wget
+else
+    echo "Une installation de curl ou wget est nécessarie pour l'execution de ce program. Voulez vous installer curl?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) sudo apt install curl; pingpong_curl; break;;   #sudo apt marche seulement sur Linux mais curl vient avec OSX donc sur OSX on n'arrivera jamais ici.
+            No ) echo "Merci pour votre participation."; exit;;
+        esac
+    done
 fi
