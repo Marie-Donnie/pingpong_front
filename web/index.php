bff7844e1e688e43670a5d966fc351576b3baa61
Josie Bealle <josiebealle@gmail.com>
2020-02-12 18:44:51 +0100

    change map

--- web/index.php
+++ web/index.php
@@ -107,41 +107,53 @@
         var map = new google.maps.Map(
                 document.getElementById('map'), {zoom: 6, center: france});
         // var marker = new google.maps.Marker({position: f, map: map});
-        $.get('https://aqueous-dusk-24314.herokuapp.com/ip/all/condensed', function(data, status){
-          data.map((dataPoint) => {
-            let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
-            if (dataPoint.type === "USER") {
-               let marker = new google.maps.Marker({position: ping, map: map,title: dataPoint.label, label: "U"})
-            } else {
-                let marker = new google.maps.Marker({position: ping, map: map,title: dataPoint.label, icon: {
-                                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}})
-            }
-          });
-        });
-
+//        $.get('https://aqueous-dusk-24314.herokuapp.com/ip/all/condensed', function(data, status){
+//          data.map((dataPoint) => {
+//            let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
+//            if (dataPoint.type === "USER") {
+//               let marker = new google.maps.Marker({position: ping, map: map,title: dataPoint.label, label: "U"})
+//            } else {
+//                let marker = new google.maps.Marker({position: ping, map: map,title: dataPoint.label, icon: {
+//                                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}})
+//            }
+//          });
+//        });
+        let polylines = [];
         $.get('https://aqueous-dusk-24314.herokuapp.com/traceroutes/all/condensed', function(data, status){
-          data.map((pingDat) => {
-              let ping = [{lat: parseFloat(pingDat.src.latitude),
-                lng: parseFloat(pingDat.src.longitude)},
-                {lat: parseFloat(pingDat.target.latitude), lng: parseFloat(pingDat.target.longitude)}];
-              let lineSymbol = {
-                path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
-              };
-              let pingPath = new google.maps.Polyline({
-                path: ping,
-                geodesic: true,
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 100 * pingDat.frequency,
-                icons: [{
-                  icon: lineSymbol,
-                  offset: '100%',
-                  scale: 3,
-                  strokeWeight: 3
-                }],
-              });
-              pingPath.setMap(map)
-          });
+
+              data.map((src) => {
+                  let ping = {lat: parseFloat(src.latitude), lng: parseFloat(src.longitude)};
+                  let marker = new google.maps.Marker({position: ping, map: map,title: dataPoint.label, label: "U"})
+
+                  marker.addListener('click', function() {
+                      polylines.map((pl) => {pl.setMap(null)});
+                      src.destinations.map((dest) => {
+                          dest.traceroute.map((hop) => {
+                              let ping = [{lat: parseFloat(hop.src.latitude),
+                                  lng: parseFloat(hop.src.longitude)},
+                                  {lat: parseFloat(hop.target.latitude), lng: parseFloat(hop.target.longitude)}];
+                              let lineSymbol = {
+                                  path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
+                              };
+                              let pingPath = new google.maps.Polyline({
+                                  path: ping,
+                                  geodesic: true,
+                                  strokeColor: '#FF0000',
+                                  strokeOpacity: 1.0,
+                                  strokeWeight: 100 * pingDat.frequency,
+                                  icons: [{
+                                      icon: lineSymbol,
+                                      offset: '100%',
+                                      scale: 3,
+                                      strokeWeight: 3
+                                  }],
+                              });
+                              polylines.push(pingPath);
+                              pingPath.setMap(map)
+                          })
+                      })
+                  });
+              })
         });
 
       }
