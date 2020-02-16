cf004cfc60f8cedefa7c4a7936b902200c430ed4
Josie Bealle <josiebealle@gmail.com>
2020-02-16 17:43:12 +0100

    load data src then dst then tr

--- web/index.php
+++ web/index.php
@@ -102,82 +102,155 @@
       <div id="map" class="map"></div>
     <script>
       // Initialize and add the map
-
-
-
       function initMap() {
-        let markers = {};
-        let tracerouteData;
-        let polylines = [];
-        var france = {lat: 48.8556, lng: 2.3522};
-        var map = new google.maps.Map(
-                document.getElementById('map'), {zoom: 6, center: france});
-        $.get('https://aqueous-dusk-24314.herokuapp.com/ip/all/', function(data, status){
-          data.map((dataPoint) => {
-            let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
-            let marker = new google.maps.Marker({position: ping, map: map,title: "ISP: " + dataPoint.isp, label: "U"})
-            markers[dataPoint.address] = marker;
+
+          let markersSrc = {};
+          let destMarkers = {};
+          let tracerouteData;
+          let polylines = [];
+          var france = {lat: 48.8556, lng: 2.3522};
+          var map = new google.maps.Map(
+              document.getElementById('map'), {zoom: 6, center: france});
+
+
+          $.get('https://aqueous-dusk-24314.herokuapp.com/sources/', function(data, status){
+              data.map((dataPoint) => {
+                  let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
+                  let marker = new google.maps.Marker({position: ping, map: map,title: "ISP: " + dataPoint.isp, label: "U"})
+                  markersSrc[dataPoint.address] = marker;
+                  marker.addListener('click', function() {
+
+                      //get destinations for sources
+                      $.get(`https://aqueous-dusk-24314.herokuapp.com/${dataPoint.address}/destinations`, function(dataDst, status){
+                          dataDst.map((dstDataPoint) => {
+                              let pingDest = {lat: parseFloat(dstDataPoint.latitude), lng: parseFloat(dstDataPoint.longitude)};
+                              let markerDest = new google.maps.Marker({position: pingDest, map: map,title: "ISP: " + dstDataPoint.isp, label: "U"})
+                              destMarkers[dstDataPoint.address] = markerDest;
+
+                              markerDest.addListener('click', function () {
+
+                                  //get traceroute
+                                  $.get(`https://aqueous-dusk-24314.herokuapp.com/${dataPoint.address}/${dstDataPoint.address}`, function(dataTr, status){
+
+                                      dataTr.map((hop) => {
+                                          let ping = [{lat: parseFloat(hop.src.latitude),
+                                              lng: parseFloat(hop.src.longitude)},
+                                              {lat: parseFloat(hop.target.latitude), lng: parseFloat(hop.target.longitude)}];
+                                          let lineSymbol = {
+                                              path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
+                                          };
+                                          let pingPath = new google.maps.Polyline({
+                                              path: ping,
+                                              geodesic: true,
+                                              strokeColor: '#FF0000',
+                                              strokeOpacity: 1.0,
+                                              strokeWeight: 3,
+                                              icons: [{
+                                                  icon: lineSymbol,
+                                                  offset: '100%',
+                                                  scale: 3,
+                                                  strokeWeight: 3
+                                              }],
+                                          });
+                                          polylines.push(pingPath);
+                                          pingPath.setMap(map);
+                                      });
+                                  });
+
+                                  //clear other destinations
+                                  Object.values(destMarkers).map((mk) => {
+                                      if (mk !== markerDest) mk.setMap(null);
+                                  });
+
+                              });
+                          })
+
+                      });
+
+                     Object.values(markersSrc).map((mk) => {
+                         if (mk !== marker) mk.setMap(null);
+                     });
+                  });
+              });
           });
 
 
 
-          $.get('https://aqueous-dusk-24314.herokuapp.com/traceroutes/all/condensed', function(data, status){
-                      Object.keys(markers).map((m) => {
-                          let mk = markers[m];
-                          mk.addListener('mouseover', function() { console.log("click2"); });
-                         });
-                       console.log("data: ", data)
-                        tracerouteData = data;
-                        Object.keys(tracerouteData).map((srcAdd) => {
-                            console.log("srcadd", srcAdd)
-                            let src = tracerouteData[srcAdd];
-                            console.log("src", src)
-          //                  let marker;
-          //                  if (!markers[src.address]) {
-          //                      console.log("add marker")
-          //                      let ping = {lat: parseFloat(src.latitude), lng: parseFloat(src.longitude)};
-          //                      marker = new google.maps.Marker({position: ping, map: map,title: "", label: "U"})
-          //                      markers[src.address] = marker;
-          //                      marker.setMap(map);
-          //                  }
-                            let marker = markers[srcAdd];
-                            console.log("marker", marker);
-                            marker.addListener('click', function() {
-                                console.log("CLICK");
-                                polylines.map((pl) => {pl.setMap(null)});
-                                Object.keys(src.dsts).map((destAdd) => {
-                                    let dest = src.dsts[destAdd]
-                                    dest.traceroute.map((hop) => {
-                                        let ping = [{lat: parseFloat(hop.src.latitude),
-                                            lng: parseFloat(hop.src.longitude)},
-                                            {lat: parseFloat(hop.target.latitude), lng: parseFloat(hop.target.longitude)}];
-                                        let lineSymbol = {
-                                            path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
-                                        };
-                                        let pingPath = new google.maps.Polyline({
-                                            path: ping,
-                                            geodesic: true,
-                                            strokeColor: '#FF0000',
-                                            strokeOpacity: 1.0,
-                                            strokeWeight: 3,
-                                            icons: [{
-                                                icon: lineSymbol,
-                                                offset: '100%',
-                                                scale: 3,
-                                                strokeWeight: 3
-                                            }],
-                                        });
-                                        polylines.push(pingPath);
-                                        pingPath.setMap(map)
-                                    })
-                                })
-                            });
-                            marker.setMap(null);
-                            marker.setMap(map);
-                        })
-                  });
-
-        });
+//        let markersSrc = {};
+//        let tracerouteData;
+//        let polylines = [];
+//        var france = {lat: 48.8556, lng: 2.3522};
+//        var map = new google.maps.Map(
+//                document.getElementById('map'), {zoom: 6, center: france});
+//
+//
+//       $.get('https://aqueous-dusk-24314.herokuapp.com/sources/', function(data, status){
+//                 data.map((dataPoint) => {
+//                   let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
+//                   let marker = new google.maps.Marker({position: ping, map: map,title: "ISP: " + dataPoint.isp, label: "U"})
+//                   markersSrc[dataPoint.address] = marker;
+//                   marker.addListener('click', function() {
+//                 });
+//
+////        //get list of all ips
+////        $.get('https://aqueous-dusk-24314.herokuapp.com/ip/all/', function(data, status){
+////          data.map((dataPoint) => {
+////            let ping = {lat: parseFloat(dataPoint.latitude), lng: parseFloat(dataPoint.longitude)};
+////            let marker = new google.maps.Marker({position: ping, map: map,title: "ISP: " + dataPoint.isp, label: "U"})
+////            markers[dataPoint.address] = marker;
+////          });
+//
+//
+//
+//          $.get('https://aqueous-dusk-24314.herokuapp.com/traceroutes/all/condensed', function(data, status){
+//                      Object.keys(markers).map((m) => {
+//                          let mk = markers[m];
+//                          mk.addListener('mouseover', function() { console.log("click2"); });
+//                         });
+//                       console.log("data: ", data)
+//                        tracerouteData = data;
+//                        Object.keys(tracerouteData).map((srcAdd) => {
+//                            console.log("srcadd", srcAdd)
+//                            let src = tracerouteData[srcAdd];
+//                            console.log("src", src)
+//                            let marker = markers[srcAdd];
+//                            console.log("marker", marker);
+//                            marker.addListener('click', function() {
+//                                console.log("CLICK");
+//                                polylines.map((pl) => {pl.setMap(null)});
+//                                Object.keys(src.dsts).map((destAdd) => {
+//                                    let dest = src.dsts[destAdd]
+//                                    dest.traceroute.map((hop) => {
+//                                        let ping = [{lat: parseFloat(hop.src.latitude),
+//                                            lng: parseFloat(hop.src.longitude)},
+//                                            {lat: parseFloat(hop.target.latitude), lng: parseFloat(hop.target.longitude)}];
+//                                        let lineSymbol = {
+//                                            path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
+//                                        };
+//                                        let pingPath = new google.maps.Polyline({
+//                                            path: ping,
+//                                            geodesic: true,
+//                                            strokeColor: '#FF0000',
+//                                            strokeOpacity: 1.0,
+//                                            strokeWeight: 3,
+//                                            icons: [{
+//                                                icon: lineSymbol,
+//                                                offset: '100%',
+//                                                scale: 3,
+//                                                strokeWeight: 3
+//                                            }],
+//                                        });
+//                                        polylines.push(pingPath);
+//                                        pingPath.setMap(map)
+//                                    })
+//                                })
+//                            });
+//                            marker.setMap(null);
+//                            marker.setMap(map);
+//                        })
+//                  });
+//
+//        });
 
       }
     </script>
